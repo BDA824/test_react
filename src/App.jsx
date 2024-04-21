788a587924ef5b2551cd2e8dd7744cfc4e2999f0
Ferney Avila <161901153+BDA824@users.noreply.github.com>
2024-04-21 03:19:56 +0000

    Targeta de Twitter terminada

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ export default function App() {
   return (
     <div className="App">
       <article className="tw-follow-card">
+        <img
+          className="tw-follow-card-img"
+          src="https://media.licdn.com/dms/image/D4D03AQGr9ucnFDH62Q/profile-displayphoto-shrink_800_800/0/1708022739210?e=1718841600&v=beta&t=OU7vMcRAI7t7uLqJR8iihnZ8eY0XQbol5EFrmgzOhzE"
+          alt="Avatar"
+        />
         <header className="tw-follow-card-info">
-          <img
-            className="tw-follow-card-img"
-            src="https://media.licdn.com/dms/image/D4D03AQGr9ucnFDH62Q/profile-displayphoto-shrink_800_800/0/1708022739210?e=1718841600&v=beta&t=OU7vMcRAI7t7uLqJR8iihnZ8eY0XQbol5EFrmgzOhzE"
-            alt="Avatar"
-          />
           <div className="tw-follow-card-userName">
-            <strong>Ferney Avila Benitez</strong>
+            <strong>Ferney Avila</strong>
             <span>@favila</span>
           </div>
         </header>
 
-        <aside>
-          <button className="tw-follow-card-button">Seguir</button>
+        <aside className="tw-follow-card-button">
+          <button>Seguir</button>
         </aside>
       </article>
     </div>
