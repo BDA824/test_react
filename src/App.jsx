e540feedee2227031b87ff1c5a59fa5ecd0e2014
Ferney Avila <161901153+BDA824@users.noreply.github.com>
2024-04-21 00:16:35 +0000

    Primeros cambios en los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import "./styles.css";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <article className="tw-follow-card">
+        <header className="tw-follow-card-info">
+          <img
+            className="tw-follow-card-img"
+            src="https://media.licdn.com/dms/image/D4D03AQGr9ucnFDH62Q/profile-displayphoto-shrink_800_800/0/1708022739210?e=1718841600&v=beta&t=OU7vMcRAI7t7uLqJR8iihnZ8eY0XQbol5EFrmgzOhzE"
+            alt="Avatar"
+          />
+          <div className="tw-follow-card-userName">
+            <strong>Ferney Avila Benitez</strong>
+            <span>@favila</span>
+          </div>
+        </header>
+
+        <aside>
+          <button className="tw-follow-card-button">Seguir</button>
+        </aside>
+      </article>
     </div>
   );
 }
